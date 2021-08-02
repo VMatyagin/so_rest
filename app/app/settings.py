8531dbb79e782e77ca9a2951d7bb0e70b7e230d2
Alexey Naiden <alexey.nayden@gmail.com>
2021-08-02 22:47:55 +0300

    Allow more localhost options

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS: List[str] = ["app.so.spb.ru", "localhost"]
+ALLOWED_HOSTS: List[str] = ["app.so.spb.ru", "localhost", "127.0.0.1"]
 
 # Application definition
 
